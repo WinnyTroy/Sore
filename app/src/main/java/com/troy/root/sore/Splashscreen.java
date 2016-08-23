14629536386fdc4a6cfcc3e70e9bd7c1b9d67483
WinnyTroy <kiraguwinnie@gmail.com>
2016-08-23 16:37:06 +0300

    upload of new activity

--- app/src/main/java/com/troy/root/sore/Splashscreen.java
+++ app/src/main/java/com/troy/root/sore/Splashscreen.java
@@ -1,7 +1,13 @@
 package com.troy.root.sore;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class Splashscreen extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        Thread load = new Thread() {
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                    Intent tranz = new Intent(getBaseContext(),landing.class);
+                    startActivity(tranz);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        load.start();
     }
+
 }
+
+
